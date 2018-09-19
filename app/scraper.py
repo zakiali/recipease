5d1d60e93c3bbd72f3f93cb46e3ffda5ce2cb81e
Zaki Ali <zakiali88@gmail.com>
2018-09-19 13:22:18 -0700

    updated templates and added scraper

---
+++ app/scraper.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+#     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+}
+
+# set some cookies to maneuver over:
+# - EU Consent in allrecipes.com.br
+COOKIES = {
+    'euConsentFailed': 'true',
+    'euConsentID': 'e48da782-e1d1-0931-8796-d75863cdfa15',
+}
+
+class SimplyRecipeScraper(object):
+    '''Get ingredients/comments from a single recipe'''
+
+    def __init__(self, url):
+        self.url = url
+        self.soup = BeautifulSoup(requests.get(self.url, headers=HEADERS, cookies=COOKIES).content, "html.parser")
+        self.get_ingredients()
+        self.get_instructions()
+        self.get_reviews()
+        
+    def get_ingredients(self):
+        self.ingredient_list = [str(li) for li in self.soup.find_all('li', class_='ingredient')]
+        
+    def get_instructions(self):
+        self.instructions = []
+        _inst = self.soup.find_all('div', {'itemprop':'recipeInstructions'})[0].find_all('p')
+        for k in _inst:
+            if k.find_next().name != 'img':
+                self.instructions.append(str(k))
+    
+    def get_reviews(self):
+        comment_url = self.url + '/comments'
+        comment_soup = BeautifulSoup(requests.get(comment_url, headers=HEADERS, cookies=COOKIES).content, "html.parser")
+        comments_html = comment_soup.find_all('div', class_="comment-text")
+        self.comments = [com.text for com in comments_html]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from .forms import RecipeURLForm
 from app import app
+from .scraper import SimplyRecipeScraper
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -8,11 +9,18 @@ def index():
     form = RecipeURLForm()
     if form.validate_on_submit():
         url = str(form.name.data)
-        return redirect(url_for('recipe'))
+        return redirect(url_for('recipe', data=url))
     return render_template('index.html',
                            title='Find a Recomendation!',
                            form=form)
     
 @app.route('/recipe', methods=['GET'])
 def recipe():
-    return render_template('recipe.html')
+    recipeurl = request.args.get('data')
+    ss = SimplyRecipeScraper(recipeurl) 
+    print(ss.ingredient_list)
+    print(ss.instructions)
+    return render_template('recipe.html',
+                            ingredients=ss.ingredient_list,
+                            instructions=ss.instructions
+                          )
