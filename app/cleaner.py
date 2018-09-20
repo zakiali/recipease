cb79bd1afd88bbfbdddedd28dd3fcdaa9fd17681
Zaki Ali <zakiali88@gmail.com>
2018-09-20 16:50:05 -0700

    create cleaner module to clean up reviews

---
+++ app/cleaner.py
@@ -0,0 +1,58 @@
+import spacy
+import pickle
+
+nlp = spacy.load('en_core_web_sm')
+
+class CommentCleaner(object):
+    '''
+        A class for cleaning up comments. 
+
+    Input: 
+        comments (list): A list of comments.
+    '''
+    def __init__(self, comments):
+        self.comments = comments    
+        self.make_sentences_and_nlp(self)
+        self.tokenizeText()
+        self.vectorize()
+
+    def make_sentences_and_nlp(self):
+        self.all_reviews = {}
+        for i, review in enumerate(self.comments):
+            self.all_reviews[i] = [sent for sent in nlp(review).sents]
+
+    def vectorize(self):
+        count_vectorizer = pickle.load(open('models/Count_vectorizer_chicken'))
+        self.all_vectorized_reviews = {}
+        for key, val in self.all_tokenized_reviews.items():
+            self.all_vectorized_reviews[key] = count_vectorizer.transform(val)
+
+    def cleanText(text):
+        text = text.strip().replace("\n", " ").replace("\r", " ")
+        text = text.replace("-", "").replace(":", "").replace("(", "").replace(")", "")
+        text = text.lower()
+        return text
+
+    def _tokenizeText(sample):
+        tokens = []
+        for tok in sample:
+            if tok.pos_ == "SPACE":
+                continue
+            if tok.lemma_ != "-PRON-":
+                tokens.append(self.cleanText(tok.lemma_))
+            else:
+                tokens.append(tok.lower_)
+        return tokens
+    
+    def tokenizeText(self):
+        self.all_tokenized_reviews = {}
+        for k, v in self.all_reviews.items():
+            token_list = []
+            for sentence in v:
+                token_list.append(self.tokenizeText(sentence))
+                while '' in token_list[-1]:
+                    token_list[-1].remove('')
+                if len(token_list[-1]) == 0:
+                    token_list.pop()
+            self.all_tokenized_reviews[k] = token_list
+
